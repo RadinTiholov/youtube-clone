c7fa7d45ba82854240c74c09896135ede0142452
RadinTiholov <radin.tiholov@mail.bg>
2022-10-10 20:58:22 +0300

    Create the base layout of the feed page

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,6 +1,22 @@
 export const Feed = () => {
     return (
-        <>
-        </>
+        <div className="container">
+            <div className="row">
+                <div className="col-3">
+                    <div className="row">
+                        <div className="col-5">
+                        </div>
+                        <div className="col text-light">
+                            <div className="d-flex" style={{height: '800px'}}>
+                                <div className="vr"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    {/* ToDo */}
+                </div>
+            </div>
+        </div>
     )
 }
