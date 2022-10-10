ca106c8daf86a6ac6ca28f13e7a2900370efe9a1
RadinTiholov <radin.tiholov@mail.bg>
2022-10-10 09:11:45 +0300

    Add basic components and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Header } from './components/Header/Header';
+import { Feed } from './components/Feed/Feed';
+import { VideoDetail } from './components/VideoDetail/VideoDetail';
+import { ChannelDetail } from './components/ChannelDetail/ChannelDetail';
+import { SearchFeed } from './components/SearchFeed/SearchFeed';
 
 function App() {
-  return (
-    <>
-    </>
-  );
+    return (
+        <div>
+            <Header />
+            <Routes>
+                <Route path='/' element={<Feed />} />
+                <Route path='/video/:id' element={<VideoDetail />} />
+                <Route path='/channel/:id' element={<ChannelDetail />} />
+                <Route path='/search/:searchTerm' element={<SearchFeed />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
